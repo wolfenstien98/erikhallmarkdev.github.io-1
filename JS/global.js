215563fb9249da1b0d7eff397762daaba11c25a4
erikhallmarkdev <erikhallmarkdev@gmail.com>
2016-10-13 12:53:33 -0700

    Design Template Finished.
    This the basic template for each of the pages on the website. I'll probably have to change around the links in the navigation section.

---
+++ JS/global.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+  $('#nav-mobile-button').click(function(){
+    $('#nav-mobile-button').toggleClass('active');
+    $('#nav-mobile').toggleClass('active');
+  });
+});
