4a7101baba9cca86d43b59e04479eb90d7738538
erikhallmarkdev <erikhallmarkdev@gmail.com>
2016-10-20 21:36:06 -0700

    Delete script.js

--- _site/js/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('#nav-mobile-button').click(function(){
-    $('#nav-mobile-button').toggleClass('active');
-    $('#nav-mobile').toggleClass('active');
-  });
-});
